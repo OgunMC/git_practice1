e1ee1219ff63c592c27dd4f760e1e1a4eb03d4ec
OgunMC <oguncalisir01@gmail.com>
2022-06-04 02:54:01 +0100

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        String result = " ";
+
+        for (int i = 0; i <result.length() ; i++) {
+          result=("Merhaba");
+
+            System.out.println(result);
+
+        }
+
+
+
+
+
+    }
+}
